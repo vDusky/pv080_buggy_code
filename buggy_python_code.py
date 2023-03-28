b64cbc48d32ae7561a78f6478f5b7045b1980497
Dušan Veľký <velky.dusko@gmail.com>
2023-03-28 18:49:54 +0200

    fixed exec urllib

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -25,7 +25,8 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
+    import urllib3 as urllib
+    #exec(f"import urllib{urllib_version} as urllib", globals())
 
     # Fetch and print the requested URL
  
